7ce37b237edd3040dc482007893b77f147609cfc
Hieu <hieusih@gmail.com>
2026-09-10 16:39:15 +0700

    feat: add ShapeTool script with configuration paths and export settings

---
+++ Assets/1 Scripts/ShapeTool.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ShapeTemplate))]
+public class ShapeTool : MonoBehaviour
+{
+    [Header("Config Paths")]
+    public string shapesJsonPath = "D:/PurusGame/solitaire-classic/assets/preload/jsons/shapes.json";
+    
+    [Header("References")]
+    public GameObject cardPrefab;
+
+    [Header("Export Settings")]
+    public float positionMultiplier = 100f;
+    public bool invertY = true;
+    public bool invertAngle = true;
+}
