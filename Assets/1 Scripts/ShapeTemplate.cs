e0862f3300aa0c20f1898274c21e83f6eb4a2415
Hieu <hieusih@gmail.com>
2026-09-10 16:07:41 +0700

    feat: map prefab and script updates

--- Assets/1 Scripts/ShapeTemplate.cs
+++ Assets/1 Scripts/ShapeTemplate.cs
@@ -3,10 +3,9 @@ using UnityEngine;
 public class ShapeTemplate : MonoBehaviour
 {
     [Header("Shape Definition (For shapes.json)")]
-    [Tooltip("ID của shape (VD: diamond_ring, cross_stack...)")]
+    [Tooltip("ID shape (VD: diamond_ring, cross_stack...)")]
     public string shapeId = "new_shape";
     
     [Header("Map Instance Data (For mapTemplates.json)")]
-    [Tooltip("Base layer khi shape này được đặt trong một Map")]
     public int baseLayer = 1;
 }
