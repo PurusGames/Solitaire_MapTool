3cf5f6bffa1eee3f47c88e0349dab1866ed9a539
Hieu <hieusih@gmail.com>
2026-09-10 15:57:57 +0700

    feat: add MapTool script for map generation

---
+++ Assets/1 Scripts/MapTool.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MapTemplate))]
+public class MapTool : MonoBehaviour
+{
+    [Header("Config Paths")]
+    public string mapsJsonPath = "D:/PurusGame/solitaire-classic/assets/preload/jsons/mapTemplates.json";
+    public string shapesJsonPath = "D:/PurusGame/solitaire-classic/assets/preload/jsons/shapes.json";
+    
+    [Header("References")]
+    public GameObject cardPrefab;
+
+    [Header("Export Settings")]
+    public float positionMultiplier = 100f;
+    public bool invertY = false;
+    public bool invertAngle = false;
+}
