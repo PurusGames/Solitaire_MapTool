a0bb4df236a0ee8ccf5765f37496b27f2c328174
Hieu <hieusih@gmail.com>
2026-09-10 18:36:32 +0700

    feat: add custom inspector for CardGizmo to adjust layer

---
+++ Assets/1 Scripts/Editor/CardGizmoEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CardGizmo)), CanEditMultipleObjects]
+public class CardGizmoEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        SerializedProperty layerProp = serializedObject.FindProperty("layer");
+
+        GUILayout.Space(5);
+        GUILayout.BeginHorizontal("box");
+        GUILayout.Label("Card Layer: " + layerProp.intValue, EditorStyles.boldLabel, GUILayout.Width(100));
+
+        if (GUILayout.Button("-", GUILayout.Width(40), GUILayout.Height(25)))
+        {
+            layerProp.intValue--;
+        }
+        if (GUILayout.Button("+", GUILayout.Width(40), GUILayout.Height(25)))
+        {
+            layerProp.intValue++;
+        }
+        GUILayout.EndHorizontal();
+
+        serializedObject.ApplyModifiedProperties();
+
+        DrawDefaultInspector();
+    }
+}
