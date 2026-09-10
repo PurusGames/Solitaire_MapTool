92a58cc31817ea55ed9304c9dc788047f702d68e
Hieu <hieusih@gmail.com>
2026-09-10 18:36:42 +0700

    feat: add custom inspector for ShapeTemplate to adjust base layer

---
+++ Assets/1 Scripts/Editor/ShapeTemplateEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ShapeTemplate)), CanEditMultipleObjects]
+public class ShapeTemplateEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        SerializedProperty baseLayerProp = serializedObject.FindProperty("baseLayer");
+
+        GUILayout.Space(5);
+        GUILayout.BeginHorizontal("box");
+        GUILayout.Label("Shape Base Layer: " + baseLayerProp.intValue, EditorStyles.boldLabel, GUILayout.Width(130));
+
+        if (GUILayout.Button("-", GUILayout.Width(40), GUILayout.Height(25)))
+        {
+            baseLayerProp.intValue--;
+        }
+        if (GUILayout.Button("+", GUILayout.Width(40), GUILayout.Height(25)))
+        {
+            baseLayerProp.intValue++;
+        }
+        GUILayout.EndHorizontal();
+
+        serializedObject.ApplyModifiedProperties();
+
+        DrawDefaultInspector();
+    }
+}
